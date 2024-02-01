7e1bff7fbaec0ac91fca064abe74fd3b289fd21f
bodduvr <156231544+bodduvr@users.noreply.github.com>
2024-02-01 08:52:11 -0500

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class InfiniteLoopExample {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("This is an infinite loop!");
+        }
+    }
+}
